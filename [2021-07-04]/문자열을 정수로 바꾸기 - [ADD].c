073c40e6719aa4c6912df7b20e5231be3a926968
uhcho2020 <dudgh1215@naver.com>
2021-07-04 17:50:31 +0900

    Update 문자열을 정수로 바꾸기 - [ADD].c

--- [2021-07-04]/문자열을 정수로 바꾸기 - [ADD].c
+++ [2021-07-04]/문자열을 정수로 바꾸기 - [ADD].c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdbool.h>
+//#include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
+//#include <string.h>
+//#include <math.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* s) {
     int answer = 0;
-    int len = strlen(s);
+    /*int len = strlen(s);
     if(s[0] == 45){
         for(int i = 1; i < len; i++){
             answer += (s[i] - 48) * pow(10, len-i-1);
@@ -23,6 +23,7 @@ int solution(const char* s) {
         for(int i = 0; i < len; i++){
             answer += (s[i] - 48) * pow(10, len-i-1);
         }
-    }
+    }*/
+    answer = atoi(s);
     return answer;
 }

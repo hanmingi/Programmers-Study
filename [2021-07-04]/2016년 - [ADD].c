4c5b3dae55317ba7b83e4e2a108f9f96dfe524b0
uhcho2020 <dudgh1215@naver.com>
2021-07-05 16:50:28 +0900

    2016년 - [UPDATE].c

--- [2021-07-04]/2016년 - [ADD].c
+++ [2021-07-04]/2016년 - [ADD].c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
 char* solution(int a, int b) {
     // 리턴할 값은 메모리를 동적 할당해주세요.
     char* answer = (char*)malloc(sizeof(char) * 3);
     int d = 0;
-    int* month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     for (int i = 0; i < a - 1; i++) {
-        int j = month[i];
-        d += j;
+        d += month[i];
     }
     d += b;
     d %= 7;
     char* day[] = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
     answer = day[d];
     return answer;
+}
+
+int main() {
+    printf("day : %s\n", solution(5, 24));
+    return 0;
 }
 // solution(5, 24) = "TUE"
 // solution(1, 1) = "FRI"

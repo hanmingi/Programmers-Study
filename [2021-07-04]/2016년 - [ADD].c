f34f02ea59f8d4fd45aaa2b7f6ccdde0d741c49d
uhcho2020 <dudgh1215@naver.com>
2021-07-05 15:54:42 +0900

    2016년 - [UPDATE].c

--- [2021-07-04]/2016년 - [ADD].c
+++ [2021-07-04]/2016년 - [ADD].c
@@ -4,35 +4,17 @@
 
 char* solution(int a, int b) {
     // 리턴할 값은 메모리를 동적 할당해주세요.
-    char* answer = (char*)malloc(sizeof(char)*3);
+    char* answer = (char*)malloc(sizeof(char) * 3);
     int d = 0;
-    for(int i = 1; i < a ; i++){ //전월까지의 일 수 계산
-        if(i == 2)
-            d += 29;
-        else if(i % 2 == 0 && i < 8)
-            d += 30;
-        else if(i % 2 == 1 && i < 8)
-            d += 31;
-        else if(i % 2 == 1 && i >= 8)
-            d += 30;
-        else if(i % 2 == 0 && i >= 8)
-            d += 31;
+    int* month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    for (int i = 0; i < a - 1; i++) {
+        int j = month[i];
+        d += j;
     }
     d += b;
-    if(d % 7 == 1)
-        answer = "FRI";
-    else if(d % 7 == 2)
-        answer = "SAT";
-    else if(d % 7 == 3)
-        answer = "SUN";
-    else if(d % 7 == 4)
-        answer = "MON";
-    else if(d % 7 == 5)
-        answer = "TUE";
-    else if(d % 7 == 6)
-        answer = "WED";
-    else
-        answer = "THU";
+    d %= 7;
+    char* day[] = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
+    answer = day[d];
     return answer;
 }
 // solution(5, 24) = "TUE"

91f9b5860949a2711a0d879b672714ea3fe46830
uhcho2020 <dudgh1215@naver.com>
2021-07-06 16:31:45 +0900

    가운데 글자 가져오기 - [UPDATE].c

--- [2021-07-05]/가운데 글자 가져오기.c
+++ [2021-07-05]/가운데 글자 가져오기.c
@@ -6,8 +6,8 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* s) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* answer = (char*)malloc(1);
     int len = strlen(s);
+    char* answer = (char*)malloc(sizeof(char) * len);
     if (len % 2 == 0) {
         answer[0] = s[len / 2 - 1];
         answer[1] = s[len / 2];

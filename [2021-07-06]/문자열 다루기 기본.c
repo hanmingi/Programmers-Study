dfa6394c031ecefdbbd9ec42eac2f9e931ae52ea
uhcho2020 <dudgh1215@naver.com>
2021-07-07 16:37:25 +0900

    문자열 다루기 기본 - [UPDATE}.c

--- [2021-07-06]/문자열 다루기 기본.c
+++ [2021-07-06]/문자열 다루기 기본.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 bool solution(const char* s) {
@@ -9,8 +10,10 @@ bool solution(const char* s) {
     if (len != 4 && len != 6)
         answer = false;
     for (int i = 0; i < len; i++) {
-        if (s[i] < 48 || s[i] > 57)
+        if (!isdigit(s[i])) {
             answer = false;
+            break;
+        }
     }
     return answer;
 }

e8c6baf7428b9745276ead1e84d2711df8023809
uhcho2020 <dudgh1215@naver.com>
2021-07-11 21:17:00 +0900

    숫자 문자열과 영단어 - [UPDATE]

--- [2021-07-09]/숫자 문자열과 영단어.c
+++ [2021-07-09]/숫자 문자열과 영단어.c
@@ -3,70 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* s) {
-    int* numbers = (int*)malloc(sizeof(int) * 50);
-    int answer = 0;
-    int len = strlen(s);
-    int s_num = 0;
-    int n_num = 0;
-    while (s_num < len) {
-        if (s[s_num] >= 48 && s[s_num] <= 57) {
-            numbers[n_num] = s[s_num] - 48;
-            s_num += 1;
+    int answer = 0, n_len = 0;
+    int s_len = strlen(s);
+    char eng[10][6] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    int* number = (int*)malloc(sizeof(int) * s_len);
+
+    for (int i = 0; i < s_len; i++) {
+        if (isdigit(s[i])) {
+            number[n_len] = s[i] - 48;
         }
-        else if (s[s_num] == 'z') {
-            numbers[n_num] = 0;
-            s_num += 4;
-        }
-        else if (s[s_num] == 'o') {
-            numbers[n_num] = 1;
-            s_num += 3;
-        }
-        else if (s[s_num] == 't') {
-            if (s[s_num + 1] == 'w') {
-                numbers[n_num] = 2;
-                s_num += 3;
-            }
-            else {
-                numbers[n_num] = 3;
-                s_num += 5;
+        else {
+            for (int j = 0; j < 10; j++) {
+                if (memcmp(&s[i], &eng[j], strlen(eng[j])) == 0) {
+                    number[n_len] = j;
+                    i += strlen(eng[j]) - 1;
+                    break;
+                }
             }
         }
-        else if (s[s_num] == 'f') {
-            if (s[s_num + 1] == 'o') {
-                numbers[n_num] = 4;
-                s_num += 4;
-            }
-            else {
-                numbers[n_num] = 5;
-                s_num += 4;
-            }
-        }
-        else if (s[s_num] == 's') {
-            if (s[s_num + 1] == 'i') {
-                numbers[n_num] = 6;
-                s_num += 3;
-            }
-            else {
-                numbers[n_num] = 7;
-                s_num += 5;
-            }
-        }
-        else if (s[s_num] == 'e') {
-            numbers[n_num] = 8;
-            s_num += 5;
-        }
-        else if (s[s_num] == 'n') {
-            numbers[n_num] = 9;
-            s_num += 4;
-        }
-        n_num++;
+        n_len++;
     }
-    for (int i = 0; i < n_num; i++) {
-        answer += numbers[i] * pow(10, n_num - i - 1);
+
+    for (int i = 0; i < n_len; i++) {
+        answer += number[i] * pow(10, n_len - i - 1);
     }
+
     return answer;
 }
 

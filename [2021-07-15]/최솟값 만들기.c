d0ba20bfc3e0903f80f2915b39a1453e913a0fcb
uhcho2020 <dudgh1215@naver.com>
2021-07-20 23:23:07 +0900

    최솟값 만들기 - [UPDATE]

--- [2021-07-15]/최솟값 만들기.c
+++ [2021-07-15]/최솟값 만들기.c
@@ -2,30 +2,33 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+int static upper(const void* first, const void* second) {
+    if (*(int*)first > * (int*)second)
+        return 1;
+    else if (*(int*)first < *(int*)second)
+        return -1;
+    else
+        return 0;
+}
+
+int static lower(const void* first, const void* second) {
+    if (*(int*)first > * (int*)second)
+        return -1;
+    else if (*(int*)first < *(int*)second)
+        return 1;
+    else
+        return 0;
+}
+
 // A_len은 배열 A의 길이입니다.
 // B_len은 배열 B의 길이입니다.
 int solution(int A[], size_t A_len, int B[], size_t B_len) {
     int answer = 0;
     // A 내림차순으로 정렬
-    for (int i = 0; i < A_len; i++) {
-        for (int j = i + 1; j < A_len; j++) {
-            if (A[i] < A[j]) {
-                int temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
-            }
-        }
-    }
+    qsort(A, A_len, sizeof(int), upper);
     //B 오름차순으로 정렬
-    for (int i = 0; i < B_len; i++) {
-        for (int j = i + 1; j < B_len; j++) {
-            if (B[i] > B[j]) {
-                int temp = B[i];
-                B[i] = B[j];
-                B[j] = temp;
-            }
-        }
-    }
+    qsort(B, B_len, sizeof(int), lower);
+
     for (int i = 0; i < A_len; i++) {
         answer += A[i] * B[i];
     }
@@ -35,6 +38,4 @@ int solution(int A[], size_t A_len, int B[], size_t B_len) {
 // [1,4,2] [5,4,4] = 29
 // [1,2] [3,4] = 10
 // 위처럼 각 하나의 숫자를 뽑아 곱한 값을 더한 것의 최솟값을 return
-// A는 오름차순, B는 내림차순으로 정렬한 후, 순서대로 원소를 곱해서 풀었으나,
-// 실행 결과는 통과하나, 효율성 테스트에서 시간초과가 나옴
-// 정렬 과정에서 2중for문을 사용하여 O(n^2)의 시간복잡도를 가진 것이 원인이라고 생각됨
+// stdlib에 포함된 qsort()라는 함수를 이용하여, 정렬의 시간을 줄임으로서 해결

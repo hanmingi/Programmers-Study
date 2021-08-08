11b87a9c522a900b2b7ede663014d1ff73a095fc
uhcho2020 <dudgh1215@naver.com>
2021-08-08 21:36:46 +0900

    Test1 - [UPDATE]

--- uhcho/[2021-08-06]/Test1.py
+++ uhcho/[2021-08-06]/Test1.py
@@ -1,33 +1,15 @@
-num = int(input())
-antennadis = []
-closeant = []
-house = []
+N = int(input())
+house = list(map(int, input().split()))
+house.sort()
 distance = 0
-
-
-# house에 num 갯수만큼 집 위치를  받고, sort로 정렬
-for i in range(num):
-    house.append(int(input()))
-
-lasthouse = house[num-1]
-
-# 마지막 집의 위치 안에 안테나가 위치함. 집의 위치와의 거리를 절댓값으로 비교하며, antenna와의 거리를 저장
 for i in house:
-    for j in range(num):
-        distance += abs(house[j] - i)
-    antennadis.append(distance)
-    closeant.append(distance)
-    distance = 0
-closeant.sort()
+    distance += i/N
+if house[round(N/2)]-distance < house[round(N/2)]-distance:
+    print(house[round(N/2)])
+else:
+    print(house[round(N/2)-1])
 
-# 안테나와의 거리를 정렬, 가장 적은 거리를 가진 안테나의 index를 받아와 그 index의 집 위치를 print
-for i in range(num):
-    if closeant[0] == antennadis[i]:
-        index= antennadis.index(closeant[0])
-        print(house[index])
-        break
-
-    """
-    처음에 안테나를 집에만 위치시킬 수 있다는 조건을 보지 못해 코드를 일부 수정함.
-    실패한 부분 : 띄워쓰기로 입력받아야 한다는 조건 만족 못함
-    """
+"""
+distance에 집 위치의 평균값을 for문으로 입력
+중간값에 가까운 2개의 집 위치를 선정하여 둘 중 거리가 더 가까운 집의 위치를 반환하도록 설계함
+"""

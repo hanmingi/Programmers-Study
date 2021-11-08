b62a43c4bd150d3263be7420ef57daec19be060f
yihan. jo <dudgh1215@naver.com>
2021-11-09 03:40:42 +0900

    F1874 - [ADD]

---
+++ uhcho/[2021-11] 1st/F1874.py
@@ -0,0 +1,25 @@
+n = int(input())
+count = 0
+stack = []
+result = []
+no_message=True
+
+for i in range(0,n):
+    x = int(input())
+
+    while count < x:
+      count += 1
+      stack.append(count)
+      result.append("+")
+
+    if stack[-1]==x:
+        stack.pop()
+        result.append("-")
+    else:
+        no_message = False
+        exit(0) #a clean exit without any errors / problems
+
+if no_message==False:
+    print("NO")
+else:
+    print("\n".join(result))

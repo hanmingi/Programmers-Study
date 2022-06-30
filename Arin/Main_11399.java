beb39b9845fdf969722bfbe851376d21a8475b5a
Arin <108451317+ArinJang@users.noreply.github.com>
2022-06-30 17:48:06 +0900

    [UPD] 정렬/11399/Arin

--- Arin/Main_11399.java
+++ Arin/Main_11399.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Main{
+public class Main_11399{
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	int N = sc.nextInt(); // 사람수 입력받기

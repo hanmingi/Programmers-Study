4027b90d63c215bf85cc75872d399d50aaad8c67
Arin <108451317+ArinJang@users.noreply.github.com>
2022-06-30 17:44:20 +0900

    [UPD] 정렬/K번째수/Arin

--- Arin/프로그래머스 : K번째수.java
+++ Arin/프로그래머스 : K번째수.java
@@ -8,13 +8,14 @@ class Solution {
         for(int i = 0; i < commands.length; i++){
             int k = 0;
 
-            int[] list = new int[commands[i][1]-commands[i][0] + 1];
+            int[] list = new int[commands[i][1]-commands[i][0] + 1]; //commands가 [2, 5, 3]일 때 5-2+1로 list배열 크기 설정
             
-            for(int j = commands[i][0]; j <= commands[i][1]; j++) {
+            for(int j = commands[i][0]; j <= commands[i][1]; j++) { //commands가 [2, 5, 3]일 때 array의 2번째부터 5번째까지 값을 list에 담는다
                 list[k++] = array[j-1];
             }
             Arrays.sort(list); // Arrays.sort() : 배열 오름차순 정렬
-            answer[n++] = list[commands[i][2]-1];
+            answer[n++] = list[commands[i][2]-1]; // answer배열에 list배열의 3번째 숫자를 담는다.
+
         }
         return answer;
     }

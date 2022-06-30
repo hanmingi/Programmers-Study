f22d4de88cd108108e4d929409d5c7a67a49916a
Arin <108451317+ArinJang@users.noreply.github.com>
2022-06-30 15:42:57 +0900

    [ADD] 정렬/K번째수/Arin

---
+++ Arin/프로그래머스 : K번째수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length]; //commands 개수만큼 배열 크기 설정
+        int n = 0;
+        
+        for(int i = 0; i < commands.length; i++){
+            int k = 0;
+
+            int[] list = new int[commands[i][1]-commands[i][0] + 1];
+            
+            for(int j = commands[i][0]; j <= commands[i][1]; j++) {
+                list[k++] = array[j-1];
+            }
+            Arrays.sort(list); // Arrays.sort() : 배열 오름차순 정렬
+            answer[n++] = list[commands[i][2]-1];
+        }
+        return answer;
+    }
+}

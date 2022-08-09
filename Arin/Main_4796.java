36427786fe82e91e3d9428ee431ab4d425ffb85d
Arin <rnernej@gmail.com>
2022-08-09 22:42:19 +0900

    [ADD] 탐욕법 / 4796 / 장아린

---
+++ Arin/Main_4796.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main_4796{ //캠핑
+
+    public static void main(String[] args) {
+        //사용가능 일수 L, 연속일수 P, 휴가일수 V 입력받기
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int L = sc.nextInt();
+            int P = sc.nextInt();
+            int V = sc.nextInt();
+
+            if(L==0) break;
+        }
+        
+    }
+
+}

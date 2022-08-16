b9a384fc088271493762a53da41787efc991674e
Arin <rnernej@gmail.com>
2022-08-16 22:28:12 +0900

    [ADD] 탐욕법/큰 수 만들기/장아린

---
+++ Arin/큰수만들기.java
@@ -0,0 +1,16 @@
+class Solution_BigNumber { // 큰 수 만들기
+    public String solution(String number, int k) {
+        String answer = "";
+        return answer;
+    }
+}
+
+public class 큰수만들기 {
+    public static void main(String[] args) {
+
+        Solution_BigNumber s = new Solution_BigNumber();
+        String number = "1924";
+        int k = 3;
+        System.out.println(s.solution(number, k));
+    }
+}

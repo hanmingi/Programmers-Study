a179204130dbe0efe0fb7cb1b1b5be06439fa6c9
Arin <rnernej@gmail.com>
2022-10-27 19:53:42 +0900

    [UPD] 문자열 / 1254 / 장아린

--- Arin/Main_1254.java
+++ Arin/Main_1254.java
@@ -7,7 +7,7 @@ public class Main_1254 { // 팰린드롬 만들기
         //String str = "qwerty";
         //String str = "oooio";
         int answer = str.length(); // 출력할 정답에 주어진 문자열의 길이 넣어놓음.
-                                   // 값이 그대로이거나 커질 예정.
+                                   // 그대로이거나 증가할 예정.
         for (int i = 0; i < str.length(); i++) {
             if (isPalindrome(str.substring(i))) // 앞글자를 하나씩 제거하며
                                                 // 나머지 문자가 팰린드롬인지 검사
@@ -18,16 +18,14 @@ public class Main_1254 { // 팰린드롬 만들기
         System.out.println(answer);
 
     }
-    
+
     // 주어진 문자열이 팰린드롬인지 판단하는 함수
     private static boolean isPalindrome(String str) {
-        System.out.println("Palindrome Test Start!!");
         int first = 0; // 문자열 첫 글자
         int last = str.length() - 1; // 문자열 마지막 글자
-        while (first <= last) { //
+        while (first <= last) { // 문자열의 처음과 마지막에서 시작해서 중간 글자까지 반복
             if (str.charAt(first) != str.charAt(last)) // 첫 글자와 마지막 글자가 다르면
                 return false;                          // 팰린드롬일 수 없으므로 false 반환
-            System.out.println("first & last : "+first+" & "+last);
             first++;
             last--;
         }

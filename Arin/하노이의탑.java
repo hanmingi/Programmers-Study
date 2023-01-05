626ccc1ade086e430c9ba347d7ffd81fe9890d54
Arin <rnernej@gmail.com>
2023-01-05 22:16:50 +0900

    [UPD] 프로그래머스 / 하노이의탑 / 장아린

--- Arin/하노이의탑.java
+++ Arin/하노이의탑.java
@@ -3,7 +3,7 @@ class Solution_towerOfHanoi {
 
     public int[][] solution(int n) {
         int[][] answer = new int[(int) Math.pow(2, n) - 1][2];
-        hanoi(n, 1, 3, 2, answer);
+        hanoi(n, 1, 2, 3, answer);
         return answer;
     }
 
@@ -11,9 +11,42 @@ class Solution_towerOfHanoi {
         if (n == 1)
             answer[index++] = new int[] { first, third };
         else {
-            hanoi(n - 1, first, second, third, answer); // 가장 큰 링을 제외하고 모두 2번으로 이동
+            hanoi(n - 1, first, third, second, answer); // 가장 큰 링을 제외하고 모두 2번으로 이동
             answer[index++] = new int[] { first, third }; // 가장 큰 링을 3번으로 이동
-            hanoi(n - 1, second, third, first, answer); // n-1 하노이의 탑을 다시 반복하기 위해 재귀호출
+            hanoi(n - 1, second, first, third, answer); // 2번탑>3번탑을 다시 반복하기 위해 재귀호출
         }
     }
 }
+
+// class Solution_towerOfHanoi {
+//     public int[][] solution(int n) {
+
+//         int[][] answer = new int[(n - 1) * 2 + 1][2];
+//         for (int i = 0; i < n - 1; i++) {
+//             answer[i][0] = 1;
+//             answer[i][1] = 2;
+//         }
+//         answer[n - 1][0] = 1;
+//         answer[n - 1][1] = 3;
+
+//         for (int i = 0; i < n - 1; i++) {
+//             answer[i + n][0] = 2;
+//             answer[i + n][1] = 3;
+//         }
+//         return answer;
+//     }
+// }
+
+// public class 하노이의탑 {
+//     public static void main(String[] args) {
+//         Solution_towerOfHanoi sol = new Solution_towerOfHanoi();
+//         int n = 3;
+//         for (int i = 0; i < (n - 1) * 2 + 1; i++) {
+//             for (int j = 0; j < 2; j++) {
+//                 int[][] a = sol.solution(n);
+//                 System.out.print(a[i][j]);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }

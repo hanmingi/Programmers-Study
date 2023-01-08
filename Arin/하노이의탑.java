07624bf390357cf52e1c1c1f909ac93f21bb7bac
Arin <rnernej@gmail.com>
2023-01-08 20:58:46 +0900

    [UPD] 프로그래머스 / 하노이의탑 / 장아린

--- Arin/하노이의탑.java
+++ Arin/하노이의탑.java
@@ -10,11 +10,15 @@ class Solution_towerOfHanoi {
     }
 
     public void hanoi(int n, int first, int second, int third, int[][] answer) {
+        System.out.println("hanoi~ n: "+n);
         if (n == 1){
-            answer[index++] = new int[] { first, third };
+            System.out.println("n=1 index: "+index+" / first: "+first+" / third: "+third);
+            answer[index++] = new int[] { first, third }; // index++ 이므로 이전 인덱스가 들어가고
+                                                          // 이후 index부터 1 증가된 값이 적용됨
         }
         else {
             hanoi(n - 1, first, third, second, answer); // 가장 큰 링을 제외하고 모두 2번으로 이동
+            System.out.println("n!1 index: "+index+" / first: "+first+" / third: "+third);
             answer[index++] = new int[] { first, third }; // 가장 큰 링을 3번으로 이동
             hanoi(n - 1, second, first, third, answer); // n-1 하노이의 탑을 다시 반복하기 위해 재귀호출
         }
@@ -24,42 +28,7 @@ class Solution_towerOfHanoi {
 public class 하노이의탑{
     public static void main(String[] args) {
         Solution_towerOfHanoi sol = new Solution_towerOfHanoi();
-        int n = 3;
+        int n = 4;
         System.out.println(Arrays.deepToString(sol.solution(n)));
     }
 }
-
-////////////////////////////////////////////////////////
-
-// class Solution_towerOfHanoi {
-//     public int[][] solution(int n) {
-
-//         int[][] answer = new int[(n - 1) * 2 + 1][2];
-//         for (int i = 0; i < n - 1; i++) {
-//             answer[i][0] = 1;
-//             answer[i][1] = 2;
-//         }
-//         answer[n - 1][0] = 1;
-//         answer[n - 1][1] = 3;
-
-//         for (int i = 0; i < n - 1; i++) {
-//             answer[i + n][0] = 2;
-//             answer[i + n][1] = 3;
-//         }
-//         return answer;
-//     }
-// }
-
-// public class 하노이의탑 {
-//     public static void main(String[] args) {
-//         Solution_towerOfHanoi sol = new Solution_towerOfHanoi();
-//         int n = 3;
-//         for (int i = 0; i < (n - 1) * 2 + 1; i++) {
-//             for (int j = 0; j < 2; j++) {
-//                 int[][] a = sol.solution(n);
-//                 System.out.print(a[i][j]);
-//             }
-//             System.out.println();
-//         }
-//     }
-// }

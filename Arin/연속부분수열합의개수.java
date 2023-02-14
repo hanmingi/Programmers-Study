3d28f2246dfd7e7727079492236a051e12306299
Arin <rnernej@gmail.com>
2023-02-15 00:49:31 +0900

    Update 연속부분수열합의개수.java

--- Arin/연속부분수열합의개수.java
+++ Arin/연속부분수열합의개수.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 class Solution_SuccessiveNumArray {
     public int solution(int[] elements) {
+
+        // 배열의 끝부분과 앞부분의 연속수열 합 구할 수 있도록 elements 2번 반복하는 배열 선언
         int[] elementsDouble = new int[elements.length * 2];
         for (int i = 0; i < elements.length; i++) {
             elementsDouble[i] = elementsDouble[i + elements.length] = elements[i];
         }
-        HashSet<Integer> tot = new HashSet<>();
 
-        for (int i = 0; i < elements.length; i++) { // 길이가 i인 연속 부분 수열
+        // 중복 허용하지 않아야 하므로 set에 담는다
+        HashSet<Integer> set_tot = new HashSet<>();
+        for (int i = 0; i < elements.length; i++) {
             for (int j = 0; j < elements.length; j++) { // 합계 시작할 인덱스
-                int num = 0;
-                for (int k = 0; k < i; k++) {
+                int num = 0; // set에 넣을 숫자
+                for (int k = 0; k < i; k++) { // k개(=i)의 합 구하기
                     num += elementsDouble[j + k];
                 }
-                tot.add(num);
+                set_tot.add(num);
             }
         }
-        int answer = tot.size();
+        int answer = set_tot.size();
         return answer;
     }
 }

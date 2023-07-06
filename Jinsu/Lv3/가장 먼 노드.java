02619d47c5ff9e0a32b4e47babeb49e4d86094e7
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-07-06 19:51:37 +0900

    Update 가장 먼 노드.java

--- Jinsu/Lv3/가장 먼 노드.java
+++ Jinsu/Lv3/가장 먼 노드.java
@@ -9,41 +9,53 @@ public class FurthestNode {
      public int solution(int n, int[][] edge) {
         int answer = 0;
 
-        //그래프 생성
+        //그래프 생성(노드생성)
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < n + 1; i++) 
+        {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < edge.length; i++) {
+
+        //간선 생성
+        for (int i = 0; i < edge.length; i++) 
+        {
             graph.get(edge[i][0]).add(edge[i][1]);
             graph.get(edge[i][1]).add(edge[i][0]);
         }
 
-        //방문 여부 확인
+        //방문 여부 확인 생성
         boolean[] visit = new boolean[n + 1];
-        visit[1] = true;
+        visit[1] = true; 
 
-        //거리 비교
+        //거리 비교 배열선언
         int[] distance = new int[n + 1];
 
         Queue<Integer> bfs = new LinkedList<>();
         bfs.add(1);
 
-        while (bfs.size() != 0) {
+        //bfs탐색 시작
+        while (bfs.size() != 0) 
+        {
             int nowNode = bfs.poll();
             ArrayList<Integer> node = graph.get(nowNode);
-            for(int i = 0; i< node.size(); i++){
-                if(visit[node.get(i)] == false){
+            for(int i = 0; i< node.size(); i++)
+            {
+                if(visit[node.get(i)] == false)
+                {
                     bfs.add(node.get(i));
                     visit[node.get(i)] = true;
                     distance[node.get(i)] = distance[nowNode] + 1;
                 }
             }
         }
+
+        //거리계산 
         Arrays.sort(distance);
         int max = distance[n];
-        for(int i : distance){
+        for(int i : distance)
+        {
+            //가장먼노드 개수파악
             if(i == max){
                 answer++;
             }
